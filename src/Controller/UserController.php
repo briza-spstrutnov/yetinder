186f4b9eeac762821e6aa1f148d8cf7fbb902fc9
Adam Bříza <briza@spstrutnov.cz>
2022-05-23 11:45:12 +0200

    Webpack

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,12 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController {
-    #[Route('/user', name: 'app_user')]
+    private $em;
+    public function __construct(EntityManagerInterface $em) {
+        $this->em = $em;
+    }
+
+    #[Route('/index', name: 'app_index')]
+    public function index(): Response {
+        return $this->render('user/index.html.twig');
+    }
+
+    #[Route('/user/register', name: 'app_user')]
     public function register(): Response {
         return $this->render('user/register.html.twig');
     }
