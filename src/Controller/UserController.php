e052abd900d45c2c16563f1be8a8c56596a61f6a
Adam Bříza <briza@spstrutnov.cz>
2022-05-24 09:23:33 +0200

    Ověřování přihlašování

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,15 +2,17 @@
 
 namespace App\Controller;
 
+use App\Form\LoginFormType;
 use App\Form\RegisterFormType;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactory;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Session;
 
 class UserController extends AbstractController {
     private $em;
@@ -25,7 +27,7 @@ class UserController extends AbstractController {
         return $this->render('user/index.html.twig');
     }
 
-    #[Route('/user/register', name: 'register_user')]
+    #[Route('/user/register', name: 'user_register')]
     public function register(Request $request): Response {
         $user = new User();
         $form = $this->createForm(RegisterFormType::class, $user);
@@ -33,8 +35,6 @@ class UserController extends AbstractController {
             'common' => ['algorithm' => 'bcrypt'],
             'memory-hard' => ['algorithm' => 'sodium'],
         ]);
-
-
 
         $form->handleRequest($request);
 
@@ -47,10 +47,46 @@ class UserController extends AbstractController {
 
             $this->em->persist($newUser);
             $this->em->flush();
-            return $this->redirectToRoute('app_index');
+            return $this->redirectToRoute('user_login');
         }
 
         return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/user/login', name: 'user_login')]
+    public function login(Request $request, UserRepository $userRepository): Response {
+        $user = new User();
+
+        $form = $this->createForm(LoginFormType::class, $user);
+
+        $factory = new PasswordHasherFactory([
+            'common' => ['algorithm' => 'bcrypt'],
+            'memory-hard' => ['algorithm' => 'sodium'],
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userInput = $form->getData();
+            $userInDb = $userRepository->findBy(['username' => $userInput->getUsername()]);
+
+            $passwordHasher = $factory->getPasswordHasher('common');
+
+            if (!$userInDb) {
+                return $this->redirectToRoute('user_login');
+            }
+
+            foreach($userInDb as $credential){
+                $password = $credential->getPassword();
+                if(!$passwordHasher->verify($password, $userInput->getPassword())){
+                    return $this->redirectToRoute('user_login');
+                }
+                dd($userInput->getPassword());
+            }
+        }
+
+        return $this->render('user/login.html.twig', [
             'form' => $form->createView()
         ]);
     }
