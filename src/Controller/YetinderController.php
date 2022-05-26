7a8a33d1426b6cb49ccbddb1f503873a9e7e6505
Adam Bříza <briza@spstrutnov.cz>
2022-05-26 08:17:56 +0200

    Alogritmus vybírání yetiho na stránku

--- src/Controller/YetinderController.php
+++ src/Controller/YetinderController.php
@@ -39,42 +39,55 @@ class YetinderController extends AbstractController {
     #[Route('/yetinder', name: 'yetinder')]
     public function yetinder(YetiRepository $yetiRepository, UserRepository $userRepository): Response {
         /* TODO:
-            1. Načíst celou db
-            2. Zkontrolovat podle id, na které už uživatel reagoval
-            3. Smazat ty, na které už uživatel reagoval
-            4. Udělat random z upraveného arraye
-            5. Vypsat na stránku
+            1. --Načíst celou db--
+            2. --Zkontrolovat podle id, na které už uživatel reagoval--
+            3. --Smazat ty, na které už uživatel reagoval--
+            4. --Udělat random z upraveného arraye--
+            5. --Vypsat na stránku--
         */
 
 
         $yetiDb = $yetiRepository->findAll();
-
-        $yeti = $yetiDb[rand(0, count($yetiDb) - 1)];
 
         $session = $this->requestStack->getSession();
         $user = $session->get('user');
         $users = $userRepository->findAll();
 
         // redirect pokud uživatel není přihlášený
-        if(!$user){
+        if (!$user) {
             return $this->redirectToRoute('user_login');
         }
 
         $userId = null;
-        foreach($user as $credential){
+        foreach ($user as $credential) {
             $userId = $credential->getId();
         }
 
         $userDb = $userRepository->find($userId);
-        $likedId = $userDb->getLiked()->getValues();
+        $liked = $userDb->getLiked()->getValues();
+        $likedId = array();
+        foreach ($liked as $x) {
+            array_push($likedId, $x->getId());
+        }
 
-        if(count($likedId) != count($yetiDb)){
-            foreach($likedId as $liked){
-                if($liked->getId() == $yeti->getId()){
-                    return $this->redirectToRoute('yetinder');
-                }
+        $yetiId = array();
+        foreach($yetiDb as $y){
+            $yetiId[] = $y->getId();
+        }
+
+        foreach($likedId as $l){
+            foreach(array_keys($yetiId, $l, true) as $key){
+                unset($yetiId[$key]);
             }
         }
+
+        $yetiNoInteraction = array();
+        foreach($yetiId as $i){
+            array_push($yetiNoInteraction, $i);
+        }
+
+        $yeti = $yetiNoInteraction[rand(0, count($yetiNoInteraction) - 1)];
+        $yeti = $yetiRepository->find($yeti);
 
         return $this->render('yetinder/yetinder.html.twig', [
             'yeti' => $yeti
@@ -90,7 +103,7 @@ class YetinderController extends AbstractController {
         $user = $session->get('user');
 
         $userId = null;
-        foreach($user as $credential){
+        foreach ($user as $credential) {
             $userId = $credential->getId();
         }
 
@@ -115,7 +128,7 @@ class YetinderController extends AbstractController {
         $user = $session->get('user');
 
         $userId = null;
-        foreach($user as $credential){
+        foreach ($user as $credential) {
             $userId = $credential->getId();
         }
 
